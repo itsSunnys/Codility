a92f299b63aec2bdd9be6cc5c594117f41a91cde
itsSunny <34824322+itsSunnys@users.noreply.github.com>
2023-10-22 15:46:05 -0400

    Create OddOccurrencesInArray.py

---
+++ OddOccurrencesInArray.py
@@ -0,0 +1,18 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+# given that each array always has odd number of elements, same value can be paired and find the value of the un-paired element
+
+# approach: O(N) traverse the array, store the values if repeated remove from set, return the left over number
+# assume given structure A will follow
+
+def solution(A):
+    # Implement your solution here
+    
+    collected = set()
+    for item in A:
+        if (item in collected):
+            collected.remove(item)
+        else:
+            collected.add(item)
+    return collected.pop()
