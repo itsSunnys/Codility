7ca7be1cc2c8c2c3f3a00ff9407f5b15f644d2d9
itsSunny <34824322+itsSunnys@users.noreply.github.com>
2023-10-22 15:55:19 -0400

    Create FrogJmp.py

---
+++ FrogJmp.py
@@ -0,0 +1,15 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+""" 
+Requirement: X is current postin, T is dest, and D is jump distance, find min jumps
+
+Approach: divide Y by X and if modulo not equal to 0 add 1
+
+"""
+
+
+def solution(X, Y, D):
+    # Implement your solution here
+
+    return (int)((Y - X)/D if (Y-X) % D == 0 else (Y - X)/D + 1)
